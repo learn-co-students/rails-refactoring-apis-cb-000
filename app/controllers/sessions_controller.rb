6d352947887657b92c5d17f095a40f51a44c22a0
Alex Segers <a.segers.dev@gmail.com>
2018-08-22 20:52:04 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
   def create
     github = GithubService.new
     github.authenticate!(ENV["GITHUB_CLIENT"], ENV["GITHUB_SECRET"], params[:code])
+    bye
+    session[:token] = github.access_token
     session[:username] = github.get_username
 
     redirect_to '/'
