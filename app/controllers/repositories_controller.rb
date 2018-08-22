f0e330b79f76cc7643c8cef50eb3977ee0759de2
Alex Segers <a.segers.dev@gmail.com>
2018-08-22 21:43:18 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,11 @@
 class RepositoriesController < ApplicationController
   def index
-    github = GithubService.new(session[:token])
+    github = GithubService.new(session)
     @repos_array = github.get_repos
   end
 
   def create
-    github = GithubService.new(session[:token])
+    github = GithubService.new(session)
     github.create_repo(params[:name])
     redirect_to '/'
   end
